a3d4ede7215ddec1cf37bb015b676456560e8060
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-06-17 18:29:27 -0700

    v2.8.1 (#5)

--- Formula/igir.rb
+++ Formula/igir.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Igir < Formula
   desc "🕹 A video game ROM collection manager to help filter, sort, patch, archive, and report on collections on any OS"
   homepage "https://igir.io/"
-  url "https://registry.npmjs.org/igir/-/igir-2.8.0.tgz"
-  sha256 "6370fae7504bdfbb5b1bef43b75f2b1ec41bb9ff757d1b206a5c43ea7c2e1398"
+  url "https://registry.npmjs.org/igir/-/igir-2.8.1.tgz"
+  sha256 "6b43524057f57a9020d2756a02f2a670194a02af5f5ea429f109ada981287ea2"
   license "GPL-3.0-or-later"
 
   livecheck do
