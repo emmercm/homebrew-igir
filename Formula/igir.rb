4ac499ae64d3929b9e0da6d7f0acfc6e8b2c0445
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-09-21 01:10:31 -0400

    Fix: broken linkages because of chdman-js and maxcso-js (#23)

--- Formula/igir.rb
+++ Formula/igir.rb
@@ -1,10 +1,8 @@
-require "language/node"
-
 class Igir < Formula
   desc "Zero-setup ROM collection manager that sorts, filters, extracts or archives, p"
   homepage "https://igir.io/"
-  url "https://registry.npmjs.org/igir/-/igir-3.0.0.tgz"
-  sha256 "0c5c8f87176695bfccaf95e211482c2e86f9f9d340d2007963f3b53d99abc141"
+  url "https://registry.npmjs.org/igir/-/igir-3.0.1.tgz"
+  sha256 "b3585f1059475cf41dd23b36cfaab2edb6aa37504c19e3582ee488ea136a9c05"
   license "GPL-3.0-or-later"
   head "https://github.com/emmercm/igir.git", branch: "main"
 
@@ -12,13 +10,11 @@ class Igir < Formula
     url :stable
   end
 
-  depends_on "libdeflate"
-  depends_on "libuv"
-  depends_on "lz4"
-  depends_on "node@20"
-  # chdman-js
-  depends_on "sdl2"
-  # maxcso-js
+  depends_on "libdeflate" # maxcso-js
+  depends_on "libuv" # maxcso-js
+  depends_on "lz4" # maxcso-js
+  depends_on "node"
+  depends_on "sdl2" # chdman-js
 
   def install
     system "npm", "install", *std_npm_args
