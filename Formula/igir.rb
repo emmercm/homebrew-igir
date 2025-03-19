d08af36010845d8e5ae13fffae442fd903c7e499
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2025-03-19 03:10:31 +0000

    v3.3.0

--- Formula/igir.rb
+++ Formula/igir.rb
@@ -1,8 +1,8 @@
 class Igir < Formula
   desc "Zero-setup ROM collection manager that sorts, filters, extracts or archives, p"
   homepage "https://igir.io/"
-  url "https://registry.npmjs.org/igir/-/igir-3.2.0.tgz"
-  sha256 "4b9460bab5b211f934b792bc1f443a22d3fd281ba1c51faaff51e8b327272cea"
+  url "https://registry.npmjs.org/igir/-/igir-3.3.0.tgz"
+  sha256 "a9e99f8c31c851dc61e5c5ddab15f0a797d7ab65d63ab668b7c44b3813ca1317"
   license "GPL-3.0-or-later"
   head "https://github.com/emmercm/igir.git", branch: "main"
 
