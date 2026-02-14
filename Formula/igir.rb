fbe75812dd8f427e4e8c7e5cf44c89325c801c06
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-14 14:26:30 -0800

    *std_npm_args(prefix: false)

--- Formula/igir.rb
+++ Formula/igir.rb
@@ -14,7 +14,7 @@ class Igir < Formula
   depends_on "python@3" => :build
 
   def install
-    system "npm", "install", "--foreground-scripts", *std_npm_args
+    system "npm", "install", "--foreground-scripts", *std_npm_args(prefix: false)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
