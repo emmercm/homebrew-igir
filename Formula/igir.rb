58d9564897908eace3817d1ef827b270272d90b0
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-14 14:43:32 -0800

    Refactor: don't `npm --foreground-scripts` (#44)

--- Formula/igir.rb
+++ Formula/igir.rb
@@ -14,7 +14,7 @@ class Igir < Formula
   depends_on "python@3" => :build
 
   def install
-    system "npm", "install", "--foreground-scripts", *std_npm_args(ignore_scripts: false), "--omit", "dev"
+    system "npm", "install", "--omit", "dev", *std_npm_args(ignore_scripts: false)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
